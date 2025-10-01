f6d02f14a467a1663f16b04a78ca563d1e5280ef
Tarunmeena0901 <meenatarun0901@gmail.com>
2025-10-01 21:46:41 +0530

    pm3 config fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,20 +2,22 @@ module.exports = {
     apps: [{
       name: 'carbon-backend',
       script: 'npm',
-      args: 'run start:prod',
-      cwd: '/root/carbon-backend',
+      args: 'run start',
+      cwd: '/Users/tarunmeena/drive/work/carbon-backend',
       instances: 1,
       autorestart: true,
       watch: false,
       env: {
         NODE_ENV: 'production',
         TZ: 'UTC',
+        DATABASE_URL: process.env.DATABASE_URL,
         HTTPS: 'true',  // Add this if your app needs to know it's behind HTTPS
         TRUST_PROXY: 'true'  // Add this for proper IP forwarding
       },
       env_production: {
         NODE_ENV: 'production',
         TZ: 'UTC',
+        DATABASE_URL: process.env.DATABASE_URL,
         HTTPS: 'true',
         TRUST_PROXY: 'true'
       },
